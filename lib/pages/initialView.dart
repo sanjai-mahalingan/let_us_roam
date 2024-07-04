84fb2f1fdc706061e34097c086d5ef58f25741e8
sanjai-mahalingan <san.mobiapp.tech@gmail.com>
2024-07-04 21:12:03 +0530

    Firebase added.

--- lib/pages/initialView.dart
+++ lib/pages/initialView.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final userSession = StateProvider<User?>((ref) => null);
 
 class InitialView extends ConsumerStatefulWidget {
   const InitialView({super.key});
@@ -10,10 +13,32 @@ class InitialView extends ConsumerStatefulWidget {
 
 class _InitialView extends ConsumerState<InitialView> {
   @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        ref.read(userSession.notifier).state = null;
+      } else {
+        ref.read(userSession.notifier).state = user;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final user = ref.watch(userSession);
+    return Scaffold(
       body: Center(
-        child: Text('Initial View'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Initial View'),
+            // ignore: unnecessary_null_comparison
+            user != null
+                ? Text(user.email as String)
+                : Text("User logged in ${user}"),
+          ],
+        ),
       ),
     );
   }
